9388f0bfee3fe51cc18eccdb7d7a228938638ba0
Ying Lu <yinglu5374@gmail.com>
2023-12-21 21:41:43 -0500

    add search

--- src/app/search/[term]/page.tsx
+++ src/app/search/[term]/page.tsx
@@ -1,23 +1,44 @@
+import MoviesCarousel from '@/app/components/movies/MoviesCarousel'
+import MoviesCarouselVertical from '@/app/components/movies/MoviesCarouselVertical'
+import { getPopularMovies, getSearchedMovies } from '@/lib/getMovies'
 import { notFound } from 'next/navigation'
 
 type Props = { params: { term: string } }
 
 // http://localhost:3000/search/[term]
 // http://localhost:3000/search/abc
-const SearchPage = ({ params }: Props) => {
+const SearchPage = async ({ params }: Props) => {
   const { term } = params
-  const termToUse = decodeURI(term)
-
-  console.log(`term=${term},termToUse=${termToUse} `)
-  // term=star%20war,termToUse=star war
 
   if (!term) {
     notFound()
   }
 
-  // API call to get populate/search movies
+  const termToUse = decodeURI(term)
 
-  return <div>SearchPage for {termToUse}</div>
+  // console.log(`term=${term},termToUse=${termToUse} `)
+  // term=star%20war,termToUse=star war
+
+  const movies = await getSearchedMovies(termToUse)
+  const popularMovies = await getPopularMovies()
+
+  return (
+    <div className='max-w-7xl mx-auto'>
+      <div className='flex flex-col space-y-4 mt-32 xl:mt-42'>
+        <h1 className='text-6xl font-bold px-10'>Results for {termToUse}</h1>
+
+        <MoviesCarouselVertical
+          movies={movies}
+          title='Movies'
+        />
+
+        <MoviesCarousel
+          movies={popularMovies}
+          title='You may also like'
+        />
+      </div>
+    </div>
+  )
 }
 
 export default SearchPage
